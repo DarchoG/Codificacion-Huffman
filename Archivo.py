58bab9a19965bdc4076e708b9b69667e10186858
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-04-11 08:32:43 -0600

    Create Archivo.py

---
+++ Archivo.py
@@ -0,0 +1,67 @@
+import os
+
+def generarDiccionario():
+    
+    Diccionario = {}
+
+    for i in range(256):
+        
+        Diccionario[chr(i)] = i
+
+    Diccionario["’"] = 256
+    print(Diccionario)
+
+    return Diccionario;   
+
+def leerArchivo(Archivo):
+    
+    try:
+
+        with open (Archivo, "r", encoding="utf8") as Documento:
+
+            Contenido = Documento.read()
+            return Contenido
+        
+    except FileNotFoundError:
+
+        print("Archivo no Encontrado");
+        return None;
+    
+def contarPalabras():
+
+    
+    #Diccionario = generarDiccionario();
+    Contador = 0
+    Diccionario = {};
+    Archivo = leerArchivo("C:/Users/GuzDa/OneDrive/Documentos/Algoritmos/Huffman/Gullivers_Travels.txt")
+    Lista = []
+
+    for i in range(len(Diccionario)):
+
+        Lista.append(0);
+
+    for i in Archivo:
+
+        print(i);
+
+        try: 
+
+            Lista[Diccionario[i]] += 1;
+
+        except:
+
+            Lista.append(1)
+            Diccionario[i] = Contador
+
+            Contador += 1
+
+    print(Diccionario);
+    print(Lista);      
+    
+    return Lista
+
+#print(leerArchivo("C:/Users/GuzDa/OneDrive/Documentos/Algoritmos/Huffman/Gullivers_Travels.txt"));    
+contarPalabras()
+
+#Diccionario = generarDiccionario()
+#print(Diccionario["’"]);
