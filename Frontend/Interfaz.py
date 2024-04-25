a0612e01559b8c43317ac23014ee142159271e97
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-04-25 07:42:31 -0600

    Update Interfaz.py

--- Frontend/Interfaz.py
+++ Frontend/Interfaz.py
@@ -1,46 +1,65 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog
 
 def inicio(ventana):
     
     ventana.title("Coodificacion Huffman");
 
+    #Obtiene la dimension de la pantalla del dispositivo en orden de adaptarse un poco a diferentes proporcones.
+
     Ancho = ventana.winfo_screenwidth();
     Largo = ventana.winfo_screenheight();
    
+    #Valores predefinidos para el tamaño y color de fondo.
+
     anchoPorcentaje = 60
-    largoPorcentaje = 60
+    largoPorcentaje = 40
+    colorFondo = '#19191a'
+
+    #Valores empleados para calcular en % de frames o widgets sobre el % total de la pantalal previamente definida.
 
     anchoVentana = int(Ancho * anchoPorcentaje / 100);
     largoVentana = int(Largo * largoPorcentaje / 100);
 
+    #Atributos de la ventana.
+
     ventana.geometry(str(anchoVentana)+ "x" + str(largoVentana))
-    ventana.configure(bg = '#19191a')
+    ventana.configure(bg = colorFondo)
 
-    textoFrame = tk.Frame(ventana, width = proporcionAncho(50, ventana, anchoVentana), height = proporcionLargo(70, ventana, largoVentana), background="red")
-    textoFrame.grid(row = 0, column = 0, sticky="nswe")
+    #Frame que encapusula el cuadro de texto que muestra el archivo cargado.
 
-    contenidoPalabras = tk.Text(textoFrame, wrap="word", state="disabled")
-    contenidoPalabras.grid(row=0, column=0, sticky="nswe")
-    contenidoPalabras.grid_rowconfigure(0, weight=1)  # Hacer que la fila se expanda
-    contenidoPalabras.grid_columnconfigure(0, weight=1)
+    textoFrame = tk.Frame(ventana, width = proporcionAncho(100, ventana, anchoVentana), height = proporcionLargo(90, ventana, largoVentana), background="red")
+    textoFrame.grid(row = 0, column = 0, sticky="nswe", padx=20, pady=10)
+    textoFrame.grid_propagate(False) # Evita desbordarse del tamaño prestablecido.
 
-    scrollbar = tk.Scrollbar(ventana, command=contenidoPalabras.yview)
-    scrollbar.grid(row=0, column=1, sticky='ns')
-    contenidoPalabras.config(yscrollcommand=scrollbar.set)
+    #Agregar una scrollbar de navegación.
 
-    graficaFrame = tk.Frame(ventana, width = proporcionAncho(50, ventana, anchoVentana), height = proporcionLargo(70, ventana, largoVentana), background="blue")
-    graficaFrame.grid(row = 0, column = 1,)
+    scrollbarx = tk.Scrollbar(textoFrame, orient=tk.HORIZONTAL)
+    scrollbarx.pack(side=tk.BOTTOM, fill=tk.X)
+    scrollbary = tk.Scrollbar(textoFrame, orient=tk.VERTICAL)
+    scrollbary.pack(side=tk.RIGHT, fill=tk.Y)
 
+    #Contenedor de texto del widget principal ################ Si es deseado alterar su contenido es requerido modificarlo con un insert ################
+
+    contenidoPalabras = tk.Text(textoFrame, wrap="none", state="normal", padx=10, pady=10, xscrollcommand=scrollbarx.set, yscrollcommand=scrollbary.set)
+    contenidoPalabras.pack(expand=True, fill=tk.BOTH)
+    contenidoPalabras.insert(tk.END, "Texto Documento: ")
+    contenidoPalabras.config(width=proporcionAncho(11.6, ventana, anchoVentana), height = proporcionLargo(3, ventana, largoVentana))
+
+    #Creacion de los botones, al cumplir con las mismas caracteristicas se emplea una misma función, la diferencia radica en algunos parametros.
+    
     botonesFrame = tk.Frame(ventana, width = proporcionAncho(100, ventana, anchoVentana), height = proporcionLargo(20, ventana, largoVentana), background='#19191a', pady = 45)
-    botonesFrame.grid(row = 1, column = 0, columnspan= 2)
+    botonesFrame.grid(row = 1, column = 0)
 
-    boton = crearBoton(botonesFrame, "Examinar", examinarArchivo);
+    boton = crearBoton(botonesFrame, "Examinar", lambda : examinarArchivo(contenidoPalabras));
     boton.grid(row = 0, column = 0,);
 
-    boton2 = crearBoton(botonesFrame, "Comprimir", None); #None es suceptible de ser remplazado por cualquier otro evento.
+    boton2 = crearBoton(botonesFrame, "Comprimir", None); 
     boton2.grid(row = 0, column = 1, padx=20);
+    
+    #None es suceptible de ser remplazado por cualquier otro evento.
+    #Es recomendado crear una funcion para eliminar el contenido previo del contendor y actualizarlo con la estrucutra de datos empleada para contar los caracteres. 
+    #El contenedor esta diseñado para no colapsar con grandes contenidos de datos, es suceptible de ser probado con textos demasiado largos.
 
     boton3 = crearBoton(botonesFrame, "Descomprimir", None); #None es susceptible de ser remplazado por cualquier otro evento.
     boton3.grid(row=0, column=2);
@@ -83,16 +102,26 @@ def crearBoton(ventana, texto, comando = None):
 
     return boton
 
-def examinarArchivo():
+def examinarArchivo(texto): #Explora el archivo y retorna su direccion absoluta, puedes comprobarlo al mandar la direccion por consola.
+
     # Abrir el explorador de archivos
     rutaArchivo = filedialog.askopenfilename()
     
-    if rutaArchivo:
-        print("Ruta del archivo seleccionado:", rutaArchivo)
+    if rutaArchivo: # Alterar el contenido del widget de texto
+          
+          with open(rutaArchivo, 'r') as archivo:
+
+            contenido = archivo.read()
+            texto.delete("1.0", tk.END)  # Limpiar el contenido anterior
+            texto.insert(tk.END, contenido)
+
+            print("Ruta del archivo seleccionado:", rutaArchivo)
+
     else:
+
         print("No se seleccionó ningún archivo.")
 
-    return rutaArchivo    
+    return rutaArchivo 
 
 ventana = tk.Tk();
 inicio(ventana)
